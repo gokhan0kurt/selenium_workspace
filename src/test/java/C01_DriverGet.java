1045b4023de34c63cbb80689935b57d5639c3be1
Gokhan Kurt <gokhan0kurt@gmail.com>
2023-05-07 10:51:08 +0300

    sample_1_selenium

---
+++ src/test/java/C01_DriverGet.java
@@ -0,0 +1,28 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class C01_DriverGet {
+    public static void main(String[] args) throws InterruptedException {
+
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://amazon.com");
+
+        System.out.println("Page Title : "+ driver.getTitle());
+
+
+        System.out.println("URL : "+driver.getCurrentUrl());
+
+        System.out.println("Page handle code (hash) : "+driver.getWindowHandle());
+        System.out.println(driver.getWindowHandles());
+
+        Thread.sleep(2000);
+        driver.close();
+
+
+    }
+}
